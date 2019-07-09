345379ed020f95ec3a876121411f0df828ba0dac
mai <e36934@jp.ibm.com>
2019-07-09 22:29:50 +0900

    edit App.js

--- JanReco/App.js
+++ JanReco/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, Image, View, TouchableHighlight } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={ require('./image/logo_janreco.jpg')} style={styles.topLogo}/>
+      <TextInput style={styles.input} placeholder="Username"/>
+      <TextInput style={styles.input} placeholder="Password"/>
+      <Text style={styles.button}>Sign In</Text>
     </View>
   );
 }
@@ -12,8 +15,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#40A698',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  topLogo: {
+    width: 400,
+    height: 400,
+    marginTop: -250,
+  },
+  input: {
+    backgroundColor:'#fff',
+    width: '60%',
+    height: 30,
+    marginBottom: 16,
   },
 });
