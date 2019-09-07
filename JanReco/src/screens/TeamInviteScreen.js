07798d5ba158ee73884024e406cf47011d9424ad
mai <e36934@jp.ibm.com>
2019-09-07 14:23:32 +0900

    edit TeamMemScreen

---
+++ JanReco/src/screens/TeamInviteScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+class TeamInviteScreen extends React.Component {
+  render() {
+    return(
+      <View>
+        <Text>あ</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default TeamInviteScreen;
