d0c916e4ec5ec884475901fcd1dfda764296d045
mai <e36934@jp.ibm.com>
2019-09-07 13:50:52 +0900

    Update TeamInfoScreen.js

--- JanReco/src/screens/TeamInfoScreen.js
+++ JanReco/src/screens/TeamInfoScreen.js
@@ -1,24 +1,50 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import Modal from 'react-native-modal';
 
 class TeamInfoScreen extends React.Component {
+  state = {
+    isModalVisible: false
+  };
+
+  toggleModal = () => {
+    this.setState({ isModalVisible: !this.state.isModalVisible });
+  };
+
   render() {
+    const {navigate} = this.props.navigation;
     return (
       <View>
         <Text style={styles.teamName}>#チーム中学</Text>
-        <View style={styles.teamMenu}>
+        <TouchableOpacity
+          style={styles.teamMenu}
+          onPress={() => navigate('TeamMem')}>
           <Icon name="sun" style={styles.icon} />
           <Text style={styles.teamMenuText}>メンバーリスト</Text>
-        </View>
-        <View style={styles.teamMenu}>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.teamMenu}
+          onPress={() => navigate('TeamInvite')}>
           <Icon name="user-plus" style={styles.icon} />
           <Text style={styles.teamMenuText}>招待</Text>
-        </View>
-        <View style={styles.teamMenu}>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.teamMenu}
+          onPress={() => this.setState({ isModalVisible: 'bottom' })}>
           <Icon name="x-square" style={styles.icon} />
           <Text style={styles.teamMenuText}>グループを退会</Text>
-        </View>
+        </TouchableOpacity>
+        <Modal
+          onBackdropPress={() => this.setState({ isModalVisible: false })}
+          isVisible={this.state.isModalVisible === 'bottom'}
+          onSwipeComplete={() => this.setState({ visibleModal: null })}
+          swipeDirection={['up', 'left', 'right', 'down']}
+          style={styles.bottomModal}>
+          <View style={styles.modal}  >
+            <Text style={styles.modalTitle}>チームを退会しますか</Text>
+          </View>
+        </Modal>
       </View>
     );
   }
@@ -47,6 +73,19 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginBottom: 10,
     paddingTop: 3,
+  },
+  bottomModal: {
+    justifyContent: 'flex-end',
+    margin: 0,
+  },
+  modal: {
+    backgroundColor: '#fff',
+    flex: 0.3,
+  },
+  modalTitle: {
+    fontSize: 20,
+    textAlign: 'center',
+    paddingTop: 13,
   },
 });
 
